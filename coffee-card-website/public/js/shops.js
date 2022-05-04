b81eaa369729de398ac0fedf6f648f9276a129cb
sunnyp95 <sunnyp95@live.com>
2022-05-04 18:27:42 -0400

    Delete coffee-card-website directory

--- coffee-card-website/public/js/shops.js
+++
@@ -1,9 +0,0 @@
-var $el = $("#slogan"), text = $el.text(),
-    words = text.split(" ");
-var html = "";
-for (var i = 0; i < words.length; i++) {
-    html += "<span>" + words[i] + " </span>";
-};
-$el.html(html).children().hide().each(function(i){
-  $(this).delay(i*1000).fadeIn(1000);
-});
